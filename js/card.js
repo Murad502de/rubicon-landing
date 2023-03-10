e3a3376c853afed120113dabfe19cf9a12c936fe
itWelt <rassulovmurad@gmail.com>
2023-03-10 15:19:03 +0300

    [main][task] fixation commit

--- js/card.js
+++ js/card.js
@@ -3,5 +3,12 @@ const onCardClicked = (card) => {
 
   setTimeout(() => {
     document.querySelector(`.card__${card}`).classList.remove('card_clicked');
+
+    if (card === 'wildberries') {
+      window.location = "https://www.wildberries.ru/seller/1205506"
+    }
+    if (card === 'ozon') {
+      window.location = "https://www.ozon.ru/seller/rubicon-jeans-938884/products"
+    }
   }, 200);
 };
--- js/index.js
+++ js/index.js
@@ -4,4 +4,8 @@ window.onload = (event) => {
   document.addEventListener('touchstart', handleTouchStart, false);
   document.addEventListener('touchend', handleTouchEnd, false);
   document.addEventListener('touchmove', handleTouchMove, false);
+
+  document.querySelectorAll('.copyright').forEach((item) => {
+    item.innerText = new Date().getFullYear() + ' © Все права защищены.';
+  });
 };
