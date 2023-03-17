8024715c90eeaad3189a2e14ec93976e16e6e542
itWelt <rassulovmurad@gmail.com>
2023-03-17 13:42:24 +0300

    [main][bug] AR-68 completed

--- js/card.js
+++ js/card.js
@@ -5,10 +5,10 @@ const onCardClicked = (card) => {
     document.querySelector(`.card__${card}`).classList.remove('card_clicked');
 
     if (card === 'wildberries') {
-      window.location = "https://www.wildberries.ru/seller/1205506"
+      window.open('https://www.wildberries.ru/seller/1205506', '_blank').focus();
     }
     if (card === 'ozon') {
-      window.location = "https://www.ozon.ru/seller/rubicon-jeans-938884/products"
+      window.open('https://www.ozon.ru/seller/rubicon-jeans-938884/products', '_blank').focus();
     }
   }, 200);
 };
